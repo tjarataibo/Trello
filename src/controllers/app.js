c370cde368750ebc348be6546aeb49df0fcba628
Tamara <tjarataibo@gmail.com>
2018-08-16 12:02:31 -0300

    Término de MVC

--- src/controllers/app.js
+++ src/controllers/app.js
@@ -1,3 +1,16 @@
 window.onload = function() {
+  window.infinite = function() {
+    const infin = window.addL();
+    window.list(infin);
+  };
 
+  window.createL = function() {
+    const create = window.infinityList();
+    window.tittle(create);
+  };
+
+  window.savv = function() {
+    const sev = window.printTittleL();
+    window.tittle(sev);
+  };
 };
