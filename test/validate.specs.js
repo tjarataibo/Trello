e349935117b9770391117bd7e6602bf1a1d37ac4
Tamara <tjarataibo@gmail.com>
2018-08-15 22:39:24 -0300

    Agregando inicios de test y js

--- test/validate.specs.js
+++ test/validate.specs.js
@@ -2,4 +2,10 @@ const assert = require('chai').assert;
 global.window = global;
 require('validate.js');
 
-
+describe('Validar formulario', () => {
+  describe('Debería verificar que exista un formulario', () => {
+    it('Debería mostrar el formulario', () => {
+      assert.equal(validateFormulario(''), false);
+    });
+  });
+}); 
